68024d22191aec7c6376fd3e9216c2b743812cd7
Priyanshi Yadav <yadavpriyanshij@gmail.com>
2026-08-07 09:35:31 +0530

    Create practice.py

---
+++ Python/Day-01-Python-Basics/practice.py
@@ -0,0 +1,43 @@
+
+# 1. Variables
+name = "Priyanshi"
+age = 21
+print(name)
+print(age)
+
+
+# 2. Data Types
+name = "Priyanshi"     # String
+age = 21               # Integer
+height = 5.4           # Float
+is_student = True      # Boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+# 3. Input and Output
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+# 4. Type Casting
+age = "21"
+
+age = int(age)
+print(age)
+print(type(age))
+
+
+# 5. Operators
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a % b)   # Modulus
+print(a ** b)  # Power
